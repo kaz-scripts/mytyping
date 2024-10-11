a0cbad88aeedf038f3a1cca49c5af82babe0b0c9
Wakka <77143406+kaz-scripts@users.noreply.github.com>
2024-10-12 03:39:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getkey() {
+function getKey() {
     const inputs = document.querySelectorAll(".mtjNowInput");
     if (inputs.length > 1) {
         return inputs[1].textContent;
@@ -8,11 +8,11 @@ function getkey() {
 }
 
 function press(key) {
-	const event = new KeyboardEvent('keydown', {key: key, bubbles: true});
-	document.dispatchEvent(event);
+    const event = new KeyboardEvent('keydown', {key: key, bubbles: true});
+    document.dispatchEvent(event);
 }
 
-setInterval(()=>{
-	key = getkey()
-	if (key) press(key)
-})
+setInterval(() => {
+    const key = getKey();
+    if (key) press(key);
+}, 0);
